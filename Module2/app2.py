19338bd9fef10a8c54a01125cd7a026b4698a07c
Rainfue <alzotey@mail.ru>
2025-03-12 10:29:43 +0300

    12.03.25 college before PEROVA

--- Module2/app2.py
+++ Module2/app2.py
@@ -16,7 +16,7 @@ from gensim.models.doc2vec import Doc2Vec
 # устройство для вычисления
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 # путь к папке с данными по модели
-model_path = 'saved_model'
+model_path = 'Module2/saved_model'
 # модель сравнивания
 model_similarity = Doc2Vec.load('Module2/doc2vec.model')
 
@@ -79,19 +79,51 @@ with col2:
     text2 = st.text_area("Введите текст второй статьи:")
 
 
-if st.button("Сравнить"):
+similarity_button = st.button(
+    label = 'Сравнить',
+)
 
-    similarity = get_similarity(text1, text2, model_similarity)
-    st.write(f'Схожесть статей: {similarity:.2f}')
+if similarity_button:
+    # проверяем, ввел ли пользователь текста:
+    if not text1 or not text2:
+        # выводим то, что пользователь должен ввести второй текст
+        st.write('Пожалуйста, введите обе статьи!')
+    # TODO сделай чтобы для пропущенной 1 и 2х статей был разный вывод
+    # если пользователь ввел обе статьи:
+    else:
+        # проверяем, ввел ли пользователь достаточно длинные тексты
+        if len(text1) < 500 or len(text2) < 500:
+            # выводим результат
+            st.write('Пожалуйста, введите статьи не менее 500 символов длиной!')
+        # если ввод корректный
+        else:
+            # получаем модель и токенизатор из кэша
+            model, tokenizer = load_summarization()
+            # получаем суммаризацию первой статьи
+            with col1:
+                summary1 = get_summary(text1, tokenizer, model, device)
+                # выводим суммаризацию
+                st.write(summary1)
+
+            # получаем суммаризацию второй статьи
+            with col2:
+                summary2 = get_summary(text2, tokenizer, model, device)
+                # выводим суммаризацию
+                st.write(summary2)
+
+            similarity = get_similarity(summary1, summary2, model_similarity)
+            st.write(f'Схожесть статей: {similarity*100:.2f}')
 
 
-    if similarity:
-        # Столбчатая диаграмма
-        st.bar_chart(
-            [[similarity*100, 100]], 
-            x_label='Схожесть',
-            color=['#4bd4ff', '#0E1117'], 
-            stack="layered", 
-            horizontal=True, 
-            use_container_width=True
-            )
+            if similarity:
+                # Столбчатая диаграмма
+                st.bar_chart(
+                    [[similarity*100, 100]], 
+                    x_label='Схожесть', 
+                    color=['#4bd4ff', '#0E1117'], 
+                    stack="layered", 
+                    horizontal=True, 
+                    use_container_width=True
+                    )
+
+
